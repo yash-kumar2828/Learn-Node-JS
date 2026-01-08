31a453c8a80b6c76389cb158f3ddbb1ff7ccc69d
yash kumar <you@example.com>
2026-01-08 22:35:49 +0530

    first file

---
+++ 0.app.js
@@ -0,0 +1,14 @@
+var fs=require('fs');
+fs.writeFileSync("YashKumar.txt","My name is yash kumar");
+
+console.log("yash kumar");
+console.log(20+20);
+function fruit(item){
+    console.log("Fruit name is : "+item);
+}
+fruit("Mango");
+
+var a=10;
+var b=20;
+console.log(a+b);
+
