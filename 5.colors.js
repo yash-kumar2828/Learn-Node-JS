03135b11b70cec3ec800102497df643478c7e340
yash kumar <you@example.com>
2026-01-13 14:14:22 +0530

    colors package

---
+++ 5.colors.js
@@ -0,0 +1,7 @@
+const colors = require('colors');
+
+console.log(colors.red("Hi this is Yash Kumar"))
+console.log(colors.yellow("Hi this is Yash Kumar"))
+console.log(colors.bgGreen("Hi this is Yash Kumar"))
+console.log(colors.green("Hi this is Yash Kumar"))
+console.log(colors.blue("Hi this is Yash Kumar"))
