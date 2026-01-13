8da550b250944118b9cf5874efc2363a2d7fb12c
yash kumar <you@example.com>
2026-01-13 14:13:35 +0530

    server

--- 4.server.js
+++ 4.server.js
@@ -1,6 +1,12 @@
 const http=require('http');
 
 http.createServer((req,res)=>{
-    res.write("<h1>My name is Yash Kumar</h1>");
+    res.write("<h1>My name is Yash Kumar Singh</h1>");
     res.end("Hello");
 }).listen(4800);
+
+
+http.createServer((req,res)=>{
+    res.write("<h1>Other Server</h1>");
+    res.end("Hello");
+}).listen(5800);
