37584757c0c96c855aa329a433b153a001db383f
yash kumar <you@example.com>
2026-01-30 11:33:39 +0530

    form

--- 12.form2.js
+++ 12.form2.js
@@ -1,6 +1,8 @@
 const http=require('http');
 const fs=require('fs');
-const { error } = require('console');
+const querystring=require('querystring');
+
+// const { error } = require('console');
 
 http.createServer((req,res)=>{
     fs.readFile('html/form.html','utf-8',(error,data)=>{
@@ -15,6 +17,28 @@ http.createServer((req,res)=>{
             res.write(data);
         }
         else if(req.url=='/submit'){
+            let dataBody=[];
+            req.on('data',(chunks)=>{
+                dataBody.push(chunks);
+            })
+            req.on('end',()=>{
+                let rawData=Buffer.concat(dataBody).toString();
+                let redableData=querystring.parse(rawData);
+                let dataString="my name is "+redableData.name+" and my email is "+redableData.email;
+                console.log(dataString);
+                // fs.writeFileSync("text/"+redableData.name+".txt",dataString);
+
+                fs.writeFile("text/"+redableData.name+".txt",dataString,"utf-8",(err)=>{;
+                if(err){
+                    res.end('internal server error')
+                    return false;
+                }
+                else{
+                    console.log ("file created");
+                }
+                
+            });
+        })
             res.write('<h1>Data Submit</h1>');
         }
         res.end();
